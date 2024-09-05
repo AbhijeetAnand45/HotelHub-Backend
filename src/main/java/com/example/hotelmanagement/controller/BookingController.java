7243e897edeb7bd83bbc427595e082b4fbe316e2
Abhijeet Anand <aabhijeet011@gmail.com>
2024-09-05 23:35:22 +0530

    Testing of all the endpoints done successfully

--- src/main/java/com/example/hotelmanagement/controller/BookingController.java
+++ src/main/java/com/example/hotelmanagement/controller/BookingController.java
@@ -17,7 +17,7 @@ public class BookingController {
     private IBookingService bookingService;
 
     @PostMapping("/book-room/{roomId}/{userId}")
-    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<Response> saveBookings(@PathVariable Long roomId,
                                                  @PathVariable Long userId,
                                                  @RequestBody Booking bookingRequest) {
@@ -29,7 +29,7 @@ public class BookingController {
     }
 
     @GetMapping("/all")
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Response> getAllBookings() {
         Response response = bookingService.getAllBookings();
         return ResponseEntity.status(response.getStatusCode()).body(response);
@@ -42,7 +42,7 @@ public class BookingController {
     }
 
     @DeleteMapping("/cancel/{bookingId}")
-    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<Response> cancelBooking(@PathVariable Long bookingId) {
         Response response = bookingService.cancelBooking(bookingId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
